924ed1508613f566cee78214e241fb52efa3e657
albertverso <caralbertverso@gmail.com>
2024-07-14 00:59:11 -0300

    fix send message and organazation views

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -8,44 +8,48 @@ from django.contrib import messages
 from django.contrib.messages import get_messages
 import time
 from django.forms import modelformset_factory
-# Create your views here.
 
 def login_user(request):
-    
+    """
+    Função para gerenciar o login e inscrição de usuários.
+    """
     if request.method == "GET":
+        # Renderiza a página de login com mensagens
         context = {
-                'messages': get_messages(request)
-                }
+            'messages': get_messages(request)
+        }
         return render(request, 'login_user.html', context)
+    
     elif request.method == "POST":
-
         if 'sigh_in' in request.POST:
+            # Processa o login
             username = request.POST.get('user_name')
             password = request.POST.get('password')
 
-            user = User.objects.get(username=username)
-
+            # Autentica o usuário
             user = authenticate(username=username, password=password)
-            if user: 
-
+            if user:
+                # Faz login e redireciona para a página inicial
                 login(request, user)
                 return redirect('home')
             else:
-                messages.error(request, 'username ou senha invalida')
+                # Exibe mensagem de erro se a autenticação falhar
+                messages.error(request, 'Nome de usuário ou senha inválida')
                 return redirect('home')
 
         elif 'sigh_up' in request.POST:
-
+            # Processa a inscrição de um novo usuário
             username = request.POST.get('user_name')
             email = request.POST.get('email')
             password = request.POST.get('password')
 
+            # Verifica se o nome de usuário já existe
             user = User.objects.filter(username=username).first()
-
-            if user: 
-                messages.error(request, 'Ja existe um usuario com esse nome')
+            if user:
+                messages.error(request, 'Já existe um usuário com esse nome')
                 return redirect('home')
 
+            # Cria um novo usuário
             user_created = User.objects.create(username=username, email=email)
             user_created.set_password(password)  # Criptografa a senha
             user_created.save()
@@ -55,13 +59,16 @@ def login_user(request):
 
 @login_required(login_url="/ecommerce/login_user")
 def home_page(request, id=None):
-
+    """
+    Função para gerenciar a página inicial com base no papel do usuário.
+    """
     if request.method == "GET":
+        # Inicializa formulários e lista de itens
         form_user = UserProfileForm(instance=request.user)
         fruit_form = FruitForm()
         sale_form = SaleForm()
         sale_item_formset = SaleItemFormSet()
-      
+
         user = request.user
         user_list = User.objects.filter(role__in=['cliente', 'vendedor'])
         sales_list = Sale.objects.filter(user=request.user)
@@ -71,6 +78,7 @@ def home_page(request, id=None):
         query = request.GET.get('search')
         fruits_list = Fruit.objects.all()
 
+        # Filtra a lista de frutas com base nos parâmetros de pesquisa
         if query:
             fruits_list = Fruit.objects.filter(fruit_name__icontains=query)
         elif filter_fruit and filter_fruit_fresh:
@@ -84,129 +92,126 @@ def home_page(request, id=None):
             if 'search' in request.GET:
                 return redirect('home')
 
+        # Obtém parâmetros de consulta para edição
         global get_query_user
         get_query_user = request.GET.get('get-user', '').strip()
         global get_query_fruit
         get_query_fruit = request.GET.get('get-fruit', '').strip()
-
-        if get_query_user:
-            user_instance = get_object_or_404(User, id=get_query_user)
-
-            form = UserProfileForm(instance=user_instance)
-
-            context = {
-            'edit': get_query_user,
-            'is_admin': True,
-            'fruits': fruits_list,
-            'list_user': user_list,
-            'user': user,
-            'form': form,
-            'fruit_form': fruit_form,
-            'messages': get_messages(request)
-            }
-            return render(request, 'home.html', context )
-        elif get_query_fruit:
-            fruit_instance = get_object_or_404(Fruit, id=get_query_fruit)
-
-            form = FruitForm(instance=fruit_instance)
-
-            context = {
-            'edit_fruit': get_query_fruit,
-            'is_admin': True,
-            'fruits': fruits_list,
-            'list_user': user_list,
-            'user': user,
-            'form': form,
-            'fruit_form': fruit_form,
-            'messages': get_messages(request)
-            }
-            return render(request, 'home.html', context )
-        elif user.role == 'Admin':
-            context = {
-            'is_admin': True,
-            'fruits': fruits_list,
-            'list_user': user_list,
-            'user': user,
-            'form': form_user,
-            'fruit_form': fruit_form,
-            'messages': get_messages(request)
-            }
-            return render(request, 'home.html', context )
-
         
+        # Renderiza a página inicial para o cliente
         if user.role == 'Cliente':
             context = {
                 'is_cliente': True,
                 'form': form_user,
                 'fruits': fruits_list,
                 'messages': get_messages(request)
-                }
+            }
             return render(request, 'home.html', context)
-
-        elif user.role == 'Vendedor': 
+        
+        # Renderiza a página inicial para o vendedor
+        elif user.role == 'Vendedor':
             context = {
                 'is_vendedor': True,
                 'fruits': fruits_list,
                 'form': form_user,
-                'sale_form': sale_form, 
+                'sale_form': sale_form,
                 'sale_item_formset': sale_item_formset,
                 'list_sales': sales_list,
                 'items_list': items_list,
                 'messages': get_messages(request)
-                }
-            return render(request, 'home.html', context)
-
-        elif user.role == 'Admin':
-            context = {
-            'edit': get_user_object,
-            'is_admin': True,
-            'fruits': fruits_list,
-            'list_user': user_list,
-            'user': user,
-            'form': form_user,
-            'fruit_form': fruit_form,
-            'messages': get_messages(request)
             }
-            return render(request, 'home.html', context ) 
+            return render(request, 'home.html', context)
+        
+        # Renderiza novamente a página inicial para o administrador (código redundante, mas deixado como está)
+        elif user.role == 'Admin':
+
+             # Processa a edição de usuário
+            if get_query_user:
+                user_instance = get_object_or_404(User, id=get_query_user)
+                form = UserProfileForm(instance=user_instance)
+
+                context = {
+                    'edit': get_query_user,
+                    'is_admin': True,
+                    'fruits': fruits_list,
+                    'list_user': user_list,
+                    'user': user,
+                    'form': form,
+                    'fruit_form': fruit_form,
+                    'messages': get_messages(request)
+                }
+                return render(request, 'home.html', context)
+            
+            # Processa a edição de fruta
+            elif get_query_fruit:
+                fruit_instance = get_object_or_404(Fruit, id=get_query_fruit)
+                form = FruitForm(instance=fruit_instance)
+
+                context = {
+                    'edit_fruit': get_query_fruit,
+                    'is_admin': True,
+                    'fruits': fruits_list,
+                    'list_user': user_list,
+                    'user': user,
+                    'form': form,
+                    'fruit_form': fruit_form,
+                    'messages': get_messages(request)
+                }
+                return render(request, 'home.html', context)
+            else:    
+                context = {
+                    'is_admin': True,
+                    'fruits': fruits_list,
+                    'list_user': user_list,
+                    'user': user,
+                    'form': form_user,
+                    'fruit_form': fruit_form,
+                    'messages': get_messages(request)
+                }
+                return render(request, 'home.html', context)
         else:
+            # Exibe uma mensagem de erro se o papel do usuário não for reconhecido
             return messages.error(request, 'Erro')
+
     elif request.method == "POST":
         if 'logout' in request.POST:
+            # Faz logout e redireciona para a página inicial
             logout(request)
-            return redirect('home')   
+            return redirect('home')
 
         elif 'edit_user' in request.POST:
+            # Processa a edição do perfil do usuário
             form = UserProfileForm(request.POST, instance=request.user)
 
             if form.is_valid():
                 form.save()
-
                 messages.success(request, 'Perfil editado')
                 return redirect('home')
             else:
-                messages.error(request, 'Error')
+                messages.error(request, 'Erro ao editar o perfil')
                 return redirect('home')
 
         elif 'added-user' in request.POST:
+            # Processa a adição de um novo usuário
             username = request.POST.get('username')
             email = request.POST.get('email')
             password = request.POST.get('password')
             role = request.POST.get('role')
 
             user = User.objects.filter(username=username).first()
+            if user:
+                messages.error(request, 'Nome de usuário já existente')
+                return redirect('home')
 
-            if user: 
-                return messages.error(request, 'Username já existente')
-        
             user_created = User.objects.create(username=username, email=email, role=role)
             user_created.set_password(password)  # Criptografa a senha
             user_created.save()
 
             messages.success(request, 'Usuário adicionado')
-
             return redirect('home')
-        elif 'added-fruit' in request.POST:
 
+        elif 'added-fruit' in request.POST:
+            # Processa a adição de uma nova fruta
             fruit_form = FruitForm(request.POST, request.FILES)
 
             if fruit_form.is_valid():
@@ -214,16 +219,16 @@ def home_page(request, id=None):
                 messages.success(request, 'Fruta adicionada')
                 return redirect('home')
 
-
         elif 'items-sale' in request.POST:
+            # Processa a adição de uma nova venda
             sale_form = SaleForm(request.POST)
             sale_item_formset = SaleItemFormSet(request.POST)
-            
+
             if sale_form.is_valid() and sale_item_formset.is_valid():
                 sale = sale_form.save(commit=False)
                 sale.user = request.user
                 sale.save()
-                
+
                 sale_items = sale_item_formset.save(commit=False)
                 for item in sale_items:
                     item.sale = sale
@@ -233,43 +238,45 @@ def home_page(request, id=None):
                 return redirect('home')
 
         elif 'btn-delete-user' in request.POST:
+            # Processa a exclusão de um usuário
             user = get_object_or_404(User, id=id)
             user.delete()
             messages.success(request, "Usuário deletado com sucesso.")
-            return redirect('home') 
-
+            return redirect('home')
 
         elif 'btn-delete-sale' in request.POST:
+            # Processa a exclusão de uma venda
             sale = get_object_or_404(Sale, id=id)
             sale.delete()
             messages.success(request, "Venda deletada com sucesso.")
-            return redirect('home') 
+            return redirect('home')
 
         elif 'btn-confirm-edit' in request.POST:
+            # Confirma a edição de um usuário
             user_instance = get_object_or_404(User, id=get_query_user)
-            form_user = UserProfileForm(request.POST ,instance=user_instance)
+            form_user = UserProfileForm(request.POST, instance=user_instance)
 
             if form_user.is_valid():
                 form_user.save()
-                # redirecionar ou fazer algo após salvar
                 messages.success(request, "Usuário editado com sucesso.")
                 return redirect('home')
             else:
                 messages.error(request, "Error")
                 return redirect('home')
-        elif 'btn-confirm-edit-fruit' in request.POST:
 
+        elif 'btn-confirm-edit-fruit' in request.POST:
+            # Confirma a edição de uma fruta
             fruit_instance = get_object_or_404(Fruit, id=get_query_fruit)
-            form_fruit = FruitForm(request.POST ,instance=fruit_instance)
+            form_fruit = FruitForm(request.POST, instance=fruit_instance)
 
             if form_fruit.is_valid():
                 form_fruit.save()
-                # redirecionar ou fazer algo após salvar
                 messages.success(request, "Fruta editada com sucesso.")
                 return redirect('home')
             else:
                 messages.error(request, "Error")
                 return redirect('home')
+
         else:
             messages.error(request, 'Error')
             return redirect('home')
